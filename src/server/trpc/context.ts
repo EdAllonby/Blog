ad16a1f5aa766fe1be8a2e38b6af6f862480b7e6
Ed Allonby <edward.allonby@gmail.com>
2022-09-27 22:25:27 +0100

    Remove comments

--- src/server/trpc/context.ts
+++ src/server/trpc/context.ts
@@ -11,6 +11,7 @@ type CreateContextOptions = Record<string, never>;
  * - testing, where we dont have to Mock Next.js' req/res
  * - trpc's `createSSGHelpers` where we don't have req/res
  **/
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createContextInner = async (opts: CreateContextOptions) => {
   return {};
 };
@@ -20,6 +21,7 @@ export const createContextInner = async (opts: CreateContextOptions) => {
  * @link https://trpc.io/docs/context
  **/
 export const createContext = async (
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   opts?: trpcNext.CreateNextContextOptions
 ) => {
   return await createContextInner({});
